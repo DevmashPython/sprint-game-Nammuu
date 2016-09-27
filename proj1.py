9004faf0fcada6094a09bd28e33ac4394366e134
Nammuu <namratasemailid@gmail.com>
2016-09-27 21:30:38 +0530

    first

---
+++ proj1.py
@@ -0,0 +1,39 @@
+import msvcrt
+
+finish=10
+count1=count2=count3=0
+
+print "press enter key to get started"
+raw_input()
+
+while count1!=10:
+	key=msvcrt.getch()
+	if key=='d':
+		count1=count1+1
+		print "_",
+	else:
+		print "error"
+		exit()
+
+if count1==10:
+	print "\n"
+while count2!=10:
+	key=msvcrt.getch()
+	if key=='s':
+		count2=count2+1
+		print "                    |"
+	else:
+		print "error"
+		exit()
+
+if count2==10 and count1==10:
+	print "                    ",
+while count3!=finish:
+	key=msvcrt.getch()
+	if key=='d':
+		count3=count3+1
+		print "_",
+	else:
+		print "error"
+		break
+print "congrats"			
